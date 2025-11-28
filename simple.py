92ffafc17a22fe39e000309d4381a25d9e813308
priyanka-bidnal <bidnalpriyanka@gmail.com>
2025-11-28 11:03:20 +0530

    save

--- simple.py
+++ simple.py
@@ -1,18 +1,15 @@
 import sys
-
 if len(sys.argv) == 4:
     principal = float(sys.argv[1])
     rate = float(sys.argv[2])
     time = float(sys.argv[3])
     print("User provided values:")
 else:
-    principal = 1000.0  
+    principal = 5000.0  
     rate = 5.0          
-    time = 2.0     
+    time = 3.0     
     print("No input provided — using default values:")
-
 simple_interest = (principal * rate * time) / 100
-
 print("\n----- SIMPLE INTEREST CALCULATOR -----")
 print("Principal:", principal)
 print("Rate of Interest (%):", rate)
